9703dfc1e66a193280c9d3e437b8f660021999e2
Jason Lokerson <cmercenary@gmail.com>
2014-07-30 12:20:32 -0700

    autowiring_error should inherit runtime_error, given its string construction

--- autowiring/autowiring_error.h
+++ autowiring/autowiring_error.h
@@ -1,19 +1,13 @@
 // Copyright (C) 2012-2014 Leap Motion, Inc. All rights reserved.
 #pragma once
-#include <exception>
+#include <stdexcept>
 
 class autowiring_error:
-  public std::exception
+  public std::runtime_error
 {
 public:
   autowiring_error(const char* what):
-    m_what(what)
+    std::runtime_error(what)
   {}
-
-private:
-  const char* m_what;
-
-public:
-  const char* what(void) const throw() override {return m_what;}
 };
 
